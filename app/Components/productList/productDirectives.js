ab3c6ae2ed4f0423ce171eae105ed8f8615d2676
DAMIR DURMO <ddurmo@gmail.com>
2016-02-19 00:29:47 -0700

    With image swap/ thumb

--- app/Components/productList/productDirectives.js
+++ app/Components/productList/productDirectives.js
@@ -1,5 +1,5 @@
 var productDirective=angular.module('productDirective',[]);
-productDirective.controller('productController',['$scope','ngCart','$stateParams', 'ProductService', 
+productDirective.controller('productController',['$scope','ngCart','$stateParams', 'ProductService',
  function($scope,ngCart,$stateParams, ProductService){
    ngCart.setTaxRate(7.5);
    ngCart.setShipping(2.99);
@@ -8,10 +8,15 @@ productDirective.controller('productController',['$scope','ngCart','$stateParams
    $scope.category=$stateParams.id;
    $scope.categoryKeyValue={category: $stateParams.id};
    console.log($scope.categoryKeyValue);
+   $scope.mainImageUrl="";
    $scope.setImage = function(imageUrl) {
       $scope.mainImageUrl = imageUrl;
       console.log($scope.mainImageUrl);
     };
+  $scope.getImage=function(){
+    console.log("Return, " , $scope.mainImageUrl);
+    return $scope.mainImageUrl;
+  };
  }
 ]);
 
@@ -27,3 +32,21 @@ productDirective.directive('ngProduct', ['ProductService',
         templateUrl: 'Components/productList/product-list.html',
     };
 }]);
+
+productDirective.directive('productImg',
+  function(){
+    return {
+        restrict : 'E',
+        transclude:true,
+        scope: {
+          product:'=',
+          imgUrl:'@'
+        },
+        link: function($scope, $element, $attrs) {
+           $scope.setImage = function(img) {
+             $scope.imgUrl=img;
+         };
+       },
+        templateUrl:'Components/productList/images.html',
+    };
+});
